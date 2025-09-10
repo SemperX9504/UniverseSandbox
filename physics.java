75a59c8af573657dda4cebfebd4ced426a15a868
whitedragon1272 <vedantpatel1272@gmail.com>
2025-09-10 23:00:52 +0530

    Create physics.java

---
+++ physics.java
@@ -0,0 +1,47 @@
+public class Physics {
+    private static final double g = 6.67430e-11;
+    private static final double mass_sun = 1.989e30;
+
+    public static double cgravity(double mass, double radius) {
+        return (g * mass) / (radius * radius);
+    }
+
+    public static double getevelo(double mass, double radius) {
+        return Math.sqrt((2 * g * mass) / radius);
+    }
+
+    public static double getovelo(double dissun) {
+        return Math.sqrt((g * mass_sun) / dissun);
+    }
+
+    public static double getdensity(double mass, double radius) {
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        return mass / volume;
+    }
+
+    public static double changemass(double mass, double factor) {
+        return mass * factor;
+    }
+
+    public static double changeradius(double radius, double factor) {
+        return radius * factor;
+    }
+
+    public static double simulatechange(double mass, double radius, double massfactor) {
+        double newmass = changemass(mass, massfactor);
+        return cgravity(newmass, radius);
+    }
+
+    public static double simulatechange(double mass, double radius, double radiusfactor, boolean changeradius) {
+        double newradius = changeradius(radius, radiusfactor);
+        return cgravity(mass, newradius);
+    }
+
+    public static double simulatechangedensity(double mass, double radius, double densityfactor) {
+        double olddensity = getdensity(mass, radius);
+        double newdensity = olddensity * densityfactor;
+        double volume = mass / newdensity;
+        double newradius = Math.cbrt((3 * volume) / (4 * Math.PI));
+        return cgravity(mass, newradius);
+    }
+}
