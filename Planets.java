d58d9d31db698a5145c03dc75681bf9ee90f538e
Tanmay Kaushal <tanmaykaushal1275@gmail.com>
2025-09-10 21:39:08 +0530

    Update Planets.java

--- Planets.java
+++ Planets.java
@@ -1,16 +1,14 @@
-
-
 public class Planets {
     private String name;
     private double mass;
     private double radius;
-    private double dissun;
-    private double ovel;
+    private vector2D dissun;
+    private vector2D ovel;
     private double rperiod;
     private double temperature;
 
     public Planets(String name,double mass,double radius,
-                   double dissun,double ovel,double rperiod,double temperature){
+                   vector2D dissun,vector2D ovel,double rperiod,double temperature){
         this.name = name;
         this.mass = mass;
         this.radius = radius;
@@ -24,11 +22,10 @@ public class Planets {
         System.out.println("Planet" + name);
         System.out.println("mass"+ mass);
         System.out.println("radius"+ radius);
-        System.out.println("distance from sun"+ dissun);
-        System.out.println("orbital velocity"+ ovel);
+        System.out.println("distance from sun"+ dissun.magnitude());
+        System.out.println("orbital velocity"+ ovel.magnitude());
         System.out.println("rotation period"+ rperiod);
         System.out.println("temperature"+ temperature);
-
     }
 
     public static void main(String[] args){
@@ -39,8 +36,8 @@ public class Planets {
             "Earth",
             5.972e24,
             6.371e6,
-            1.496e11,
-            29780.0,
+            new vector2D(1.496e11,0.0),
+            new vector2D(0.0,29780.0),
             24.0,
             15.0
         );
