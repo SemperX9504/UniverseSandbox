f67df08896d231e8e467afe45d02eac9a5c64e67
SemperX9504 <jenishvasava9504@gmail.com>
2025-09-10 23:02:30 +0530

    Proper

--- SolarSystem.java
+++ SolarSystem.java
@@ -1,10 +1,12 @@
 import java.util.*;
-import java.swing.*;
 
 import javax.swing.JFrame;
 
 public class SolarSystem {
-    JFrame frame = new JFrame("Solar System");
-    frame.setDefaultCloseOperation(JFrame.EXITONCLOSE);
-    
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Solar System");
+        frame.setDefaultCloseOperation(JFrame.EXITONCLOSE);
+
+    }
+
 }
